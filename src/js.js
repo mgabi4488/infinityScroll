f51753bbe039025b3f43add00e2b2b9964686db1
Mirzakandov Gabriel <mirzakandovgabriel@gmail.com>
2020-09-28 16:00:19 +0300

    Converted to es6 + added custom controller

--- src/js.js
+++ src/js.js
@@ -25,53 +25,54 @@ class InfinityScroll {
 		// 
 		let _ = this;
 		// 
-		this.el = typeof settings.el === 'object' ? settings.el : document.querySelector(settings.el);
+		_.setEl = {
+			isRoot: true,
+			el: settings.el,
+			trigger: settings.triggerEvent,
+		};
 		// 
-		if ( this.el == null ) throw 'Missing main object to initialize the plugin on';
+		if ( settings.customController?.el ) {
+			_.setEl = {
+				isRoot: false,
+				el: settings.customController.el,
+				trigger: settings.customController.triggerEvent,
+			};
+		}
 		// wrap the user element with the plugin element
-		this.wrap(this.el);
-		// 
-		this.el.addEventListener('scroll', function(ev){
-			let offsetHeight = ev.target.offsetHeight; // element height
-			let scrollTop = ev.target.scrollTop; // pixels scrolled from element top
-			let scrollHeight = ev.target.scrollHeight;// pixels of the whole element.
-			// 
-			if ( (offsetHeight + scrollTop) == scrollHeight ) {
-				// 
-				_.handleProgress('fetching');
-				// 
-				fetch(_.getRequestObject())
-				// check the responce status
-				.then(( response ) => {
-					// if the responce has faild we show error
-					if ( !response.ok ) {
-						throw new Error('something wernt wrong, status code: ' + response.status);
-					}
-					// convert the responce to json
-					return response.json();
-				})
-				// finish the responce
-				.then(( data ) => {
-					// finish the loading proccess
-					_.handleProgress('done');
-					// run fail callback
-					if ( _.getOption('onRequestSuccess') ) _.getOption('onRequestSuccess')(data);
-				})
-				// show 
-				.catch(( error ) => {
-					// even if there is error finish the loading procces
-					_.handleProgress('done');
-					// run fail callback
-					if ( _.getOption('onRequestFail') ) _.getOption('onRequestFail')(error);
-					// show the error in console
-					console.error('Error:', error);
-				});
-				// run custom callback
-				if ( _.getOption('onRequestStart') ) _.getOption('onRequestStart')(this.el);
-			}
-		});
+		_.wrap(_.el);
 		// save the 
-		this.el.InfinityScroll = this.settings;
+		_.el.InfinityScroll = _.settings;
+	}
+
+	/**
+	 *
+	 */
+	set setEl( settings ) {
+		// 
+		settings.el = typeof settings.el === 'object' ? settings.el : document.querySelector(settings.el);
+		// 
+		if ( settings.el == null ) throw 'Missing main to initialize the plugin on';
+		// 
+		if ( settings.isRoot ) {
+			this.el = settings.el;
+		// 
+		} else {
+			this.settings.customController.el = settings.el;
+		}
+		// 
+		this.setTrigger = settings;
+	}
+
+	/**
+	 *
+	 */
+	set setTrigger( settings ) {
+		//
+		let triggers = this.getSupportedTriggers(settings.trigger);
+		//
+		for ( let i = 0; i < triggers.length ; i++ ) {
+			settings.el.addEventListener(triggers[i],this.events[triggers[i]]);
+		}
 	}
 
 	/**
@@ -92,10 +93,25 @@ class InfinityScroll {
 	/**
 	 *
 	 */
+	getSupportedTriggers( userTriggers ) {
+		//
+		let triggers = [];
+		userTriggers = userTriggers.split(' ');
+		//
+		for ( let i = 0; i < userTriggers.length ; i++ ) {
+			// 
+			if ( !this.events?.[userTriggers[i]] ) continue;
+			// 
+			triggers.push(userTriggers[i]);
+		}
+		// 
+		return triggers;
+	}
+
+	/**
+	 *
+	 */
 	get defSettings() {
-		// 
-		let _ = this;
-		// 
 		return {
 			el: null,
 			dataPath: null,
@@ -106,7 +122,9 @@ class InfinityScroll {
 			onDataEnd: null,
 			requestObject: false,
 			customLoadingAnimation: false,
-			disableLoadingAnimation: false
+			disableLoadingAnimation: false,
+			triggerEvent: 'scroll',
+			customController: false
 		};
 	}
 
@@ -122,100 +140,6 @@ class InfinityScroll {
 	 */
 	get object() {
 		return this.el;
-	}
-
-	/**
-	 * The method is wrapping the selected element that the user wanted to add
-	 * the infinity scroll to
-	 */
-	wrap( el ) {
-		// create new dom element
-		this.wrapper = document.createElement('div');
-		this.wrapper.classList.add(this.nameSpace + '-wrapper');
-		// wrap the user element with the new created wrapper
-		el.parentNode.appendChild(this.wrapper);
-		this.wrapper.appendChild(el);
-	}
-
-	/**
-	 *
-	 */
-	setProperties( el ) {
-		// 
-		let _ = this;
-		// 
-		_.nameSpace = 'i-f';
-		// 
-		_.inProgress = false;
-		// 
-		_.methods = {
-			setOption: function( optionName, value ) {
-				// security - validate option supports
-				if ( !_.settings?.[optionName] ) throw 'The option you are trying to apply is not supported';
-				// set the option
-				_.settings[optionName] = value;
-			}
-		};
-		_.events = {
-
-		};
-	}
-
-	/**
-	 * The method is returning the option
-	 */
-	getOption( optionName ) {
-		// 
-		let option  = this.settings?.[optionName];
-		// security - validate option supports
-		if ( typeof option === 'undefined' ) throw 'The option you are trying to apply is not supported';
-		// return the option
-		return option;
-	}
-
-	/**
-	 * The method is returning the request object
-	 */
-	getRequestObject() {
-		// if the user has custom request object use it
-		if ( this.getOption('requestObject') ) return this.getOption('requestObject');
-		// use default data
-		let requestObject = new Request(this.path, {
-			method: 'GET',
-			headers: new Headers(), // just set empty headers
-			mode: 'cors',
-			cache: 'default',
-		});
-		// return the default request object
-		return requestObject;
-	}
-
-	/**
-	 *
-	 */
-	handleProgress( status ) {
-		// 
-		switch ( status ) {
-			case 'fetching':
-				// 
-				this.inProgress = true;
-				// 
-				if ( !this.getOption('disableLoadingAnimation') ) {
-					// 
-					this.loadingAnimation = this.getLoadingAnimation;
-					// 
-					this.wrapper.appendChild(this.loadingAnimation);
-				}
-				break; 
-			case 'done':
-				// 
-				this.inProgress = false;
-				// 
-				if ( !this.getOption('disableLoadingAnimation') ) {
-					this.loadingAnimation.remove();
-				}
-				break;
-		}
 	}
 
 	/**
@@ -253,10 +177,169 @@ class InfinityScroll {
 		 * Returns true if it is a DOM node
 		 * Solution: https://stackoverflow.com/a/384380/6352916
 		 */
-		function isNode( o ) {
+		isNode = ( o ) => {
 			return (
 				typeof Node === "object" ? o instanceof Node : o && typeof o === "object" && typeof o.nodeType === "number" && typeof o.nodeName === "string"
 			);
 		}
+	}
+
+	/**
+	 * The method is returning the request object
+	 */
+	get getRequestObject() {
+		// if the user has custom request object use it
+		if ( this.getOption('requestObject') ) return this.getOption('requestObject');
+		// use default data
+		let requestObject = new Request(this.path, {
+			method: 'GET',
+			headers: new Headers(), // just set empty headers
+			mode: 'cors',
+			cache: 'default',
+		});
+		// return the default request object
+		return requestObject;
+	}
+
+	/**
+	 * The method is wrapping the selected element that the user wanted to add
+	 * the infinity scroll to
+	 */
+	wrap( el ) {
+		// create new dom element
+		this.wrapper = document.createElement('div');
+		this.wrapper.classList.add(this.nameSpace + '-wrapper');
+		// wrap the user element with the new created wrapper
+		el.parentNode.appendChild(this.wrapper);
+		this.wrapper.appendChild(el);
+	}
+
+	/**
+	 *
+	 */
+	setProperties() {
+		// 
+		let _ = this;
+		// 
+		_.nameSpace = 'i-f';
+		// 
+		_.inProgress = false;
+		// 
+		_.events = {
+			scroll: ( e ) => {
+				let offsetHeight = e.target.offsetHeight; // element height
+				let scrollTop = e.target.scrollTop; // pixels scrolled from element top
+				let scrollHeight = e.target.scrollHeight;// pixels of the whole element.
+				// 
+				if ( (offsetHeight + scrollTop) == scrollHeight ) {
+					// 
+					_.handleProgress('fetching');
+					// 
+					_.events.fetchNextPage();
+					// run custom callback
+					if ( _.getOption('onRequestStart') ) _.getOption('onRequestStart')(this.el);
+				}
+			},
+			click: ( e ) => {
+				// 
+				_.handleProgress('fetching');
+				// 
+				_.events.fetchNextPage();
+				// run custom callback
+				if ( _.getOption('onRequestStart') ) _.getOption('onRequestStart')(this.el);
+			},
+			fetchNextPage: () => {
+				// 
+				fetch(_.getRequestObject)
+				// check the responce status
+				.then(( response ) => {
+					// if the responce has faild we show error
+					if ( !response.ok ) {
+						throw new Error('something wernt wrong, status code: ' + response.status);
+					}
+					// convert the responce to json
+					return response.json();
+				})
+				// finish the responce
+				.then(( data ) => {
+					// finish the loading proccess
+					_.handleProgress('done');
+					// run fail callback
+					if ( _.getOption('onRequestSuccess') ) _.getOption('onRequestSuccess')(data);
+				})
+				// show 
+				.catch(( error ) => {
+					// even if there is error finish the loading procces
+					_.handleProgress('done');
+					// run fail callback
+					if ( _.getOption('onRequestFail') ) _.getOption('onRequestFail')(error);
+					// show the error in console
+					console.error('Error:', error);
+				});
+			}
+		};
+	}
+
+	// 
+	destroy() {
+		this.wrapper.parentNode.appendChild(this.el);
+		this.wrapper.remove();
+		this.el.InfinityScroll = null;
+		// get the trigger events
+		let triggers = this.getSupportedTriggers();
+		// remove all of the events
+		for ( let i = 0; i < triggers.length ; i++ ) {
+			this.el.removeEventListener(triggers[i],this.events[triggers[i]]);
+		}
+	}
+
+	/**
+	 *
+	 */
+	handleProgress( status ) {
+		// 
+		switch ( status ) {
+			case 'fetching':
+				// 
+				this.inProgress = true;
+				// 
+				if ( !this.getOption('disableLoadingAnimation') ) {
+					// 
+					this.loadingAnimation = this.getLoadingAnimation;
+					// 
+					this.wrapper.appendChild(this.loadingAnimation);
+				}
+				break; 
+			case 'done':
+				// 
+				this.inProgress = false;
+				// 
+				if ( !this.getOption('disableLoadingAnimation') ) {
+					this.loadingAnimation.remove();
+				}
+				break;
+		}
+	}
+
+	/**
+	 * The method is returning the option
+	 */
+	getOption( optionName ) {
+		// 
+		let option  = this.settings?.[optionName];
+		// security - validate option supports
+		if ( typeof option === 'undefined' ) throw 'Trying to apply to unsupported method option';
+		// return the option
+		return option;
+	}
+
+	/**
+	 *
+	 */
+	setOption( optionName, value ) {
+		// security - validate option supports
+		if ( !this.settings?.[optionName] ) throw 'Trying to apply to unsupported method option';
+		// set the option
+		this.settings[optionName] = value;
 	}
 }
